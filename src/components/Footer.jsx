d9feab1c52d09ae7e3a9f2cf089f56ff02c862a1
Julian <julian.silveira.annoni@gmail.com>
2025-08-21 20:37:36 -0300

    nuevos cambios

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,12 +1,10 @@
 export default function Footer() {
   return (
-    <div className="relative w-full h-screen flex gap-8 align-center justify-center backdrop-blur-3xl bg-gray-800 text-white mt-10">
-    {/* <img className='absolute w-full h-full' src="../../public/52741.png"/> */}
-    <div className="flex gap-5 pt-2">
-    <p className="text-xs">© JSA</p>
-    <p className="text-xs">Hecho por Julián Silveira A.</p>
-    </div>
-    </div>
+    <footer className="w-full bg-gray-800 backdrop-blur-3xl text-white py-2">
+      <div className="flex gap-5 justify-center">
+        <p className="text-xs">© JSA</p>
+        <p className="text-xs">Hecho por Julián Silveira A.</p>
+      </div>
+    </footer>
   )
 }
-
