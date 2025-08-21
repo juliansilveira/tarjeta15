9ff2b9ca17335bef9f5c9b9b4b4eb0783a2aaac0
Julian <julian.silveira.annoni@gmail.com>
2025-08-21 20:58:12 -0300

    cambio de fuente2

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,9 +1,9 @@
 export default function Footer() {
   return (
-    <footer className="w-full bg-gray-800 backdrop-blur-3xl text-white py-2">
+    <footer className="w-full bg-gray-800 backdrop-blur-3xl text-white py-1 text-[8px]">
       <div className="flex gap-5 justify-center">
-        <p className="text-xs">© JSA</p>
-        <p className="text-xs">Hecho por Julián Silveira A.</p>
+        <p className="">© JSA</p>
+        <p className="">Hecho por Julián Silveira A.</p>
       </div>
     </footer>
   )
