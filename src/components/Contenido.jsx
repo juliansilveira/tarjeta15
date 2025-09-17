ea6f393038e101256fa528864b08d9545006d208
Julian <julian.silveira.annoni@gmail.com>
2025-09-17 10:05:04 -0300

    nuevas fuentes

--- src/components/Contenido.jsx
+++ src/components/Contenido.jsx
@@ -2,15 +2,16 @@ import React from "react";
 
 export default function Contenido() {
   return (<>
-    <div className="grid place-items-center text-center font-bold text-[#E3E6F3] mt-16 mx-1 p-8 rounded-2xl shadow-lg bg-[#000D42]/65 border">
-      <h1 className="font-edu-cursive text-3xl">Te invito a Festejar mis 15 Años</h1>
+    <div className="grid place-items-center text-center text-[#E3E6F3] mt-16 mx-1 p-8 rounded-2xl shadow-lg bg-[#000D42]/65 border">
+      <h1 className="font-chicle text-3xl underline">Te invito a Festejar mis 15 Años</h1>
       <h1 className="mt-6 text-6xl font-extrabold font-vibes">Mia</h1>
     </div>
-    <div className="bg-[#18017b]/60 text-white flex justify-around items-center p-2 rounded-xl m-12 font-ibm border-3 border-white">
-    <h3  className="">Viernes <p className="text-3xl font-bold mx-2 my-2">16</p> de Enero</h3>
-    <div className="border-l-4 font-bold">
-    <p className="ml-2">San Martín 370</p>
-    <p className="ml-7">22 a 5 hs</p>
+    <div className="bg-[#18017b]/60 text-white flex justify-around items-center p-2 rounded-xl m-12 border-3 border-white font-dancing-script text-lg">
+    <h3  className="">Viernes <p className="text-3xl font-bold mx-2">16</p> de Enero</h3>
+    <div className="border-3 pt-20 rounded-3xl"></div>
+    <div className="flex flex-col justify-center items-center">
+    <p className="text-xl">San Martín 370</p>
+    <p className="text-xl">22 a 5 hs</p>
     </div>
     </div>
     </>
